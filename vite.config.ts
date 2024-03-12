2fb49d4dc87cb37880e15d1e519517d6828d1dc9
Tetiana Protas <protas.tb@gmail.com>
2024-03-13 00:17:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/endless-game/'
+  base: './',
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
